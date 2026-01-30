ef6b3a03db32497df816cefe06b8f339f690734f
Li-Yu, Yang <tony830802@gmail.com>
2026-01-30 12:35:47 +0800

    feat: add static deploy

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  output: "export",
+  basePath: process.env.NODE_ENV === "production" ? "/interactive-blackboard" : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
