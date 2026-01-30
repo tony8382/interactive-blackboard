daacf17ae5be57a7fb9185bd888c18d0ec7c1e06
Li-Yu, Yang <tony830802@gmail.com>
2026-01-30 12:41:37 +0800

    del: remove test file for bad words filtering.

--- test-badwords.js
+++
@@ -1,8 +0,0 @@
-try {
-    const Filter = require('bad-words');
-    const filter = new Filter();
-    console.log(filter.clean("Don't be an ash0le"));
-    console.log("Success");
-} catch (e) {
-    console.error(e);
-}
